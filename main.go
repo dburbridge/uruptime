8da7dc7df7a0cf9eb9ae9b963b4cdad4726a5fc4
dburbridge <daniel.burbridge@publishingtechnology.com>
2017-09-11 09:31:53 +0100

    var

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
                         if log.Type == 1 {
                             t := strconv.Itoa(log.Duration)
                             i, _ := strconv.ParseInt(log.LogDateTime, 10, 64)
-                            tm string = time.Unix(i, 0)
+                            var tm string = time.Unix(i, 0)
                             w.Write([]byte(monitor.Friendly_name + " : " + tm + " : "+ t + "\n"))
                             
                         }
